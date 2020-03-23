f997cc95be188b2aea2adec36c8204c8da189b14
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-23 10:53:26 -0700

    Update card.py

--- card.py
+++ card.py
@@ -1,12 +1,17 @@
 import textwrap
 class Card:
-	def __init__(self, name, value):
+	def __init__(self, name, card_type, value):
 		self._name = name
+		self._card_type = card_type
 		self._value = value
-
+		
 	@property
 	def name(self):
 		return self._name
+
+	@property
+	def card_type(self):
+		return self._card_type
 
 	@property
 	def value(self):
@@ -21,3 +26,11 @@ class Card:
 								you can only change the value of a high ace,
 								you tried to change the value of '{self.name}'
 								that has a value of '{self.value}'"""))
+	
+	def __str__(self):
+		return f"{self.name}, {self.card_type}, {self.value}"
+
+	def __eq__(self, other_card):
+		
+		return (self.card_type == other_card.card_type and
+			 	self.value == other_card.value)
