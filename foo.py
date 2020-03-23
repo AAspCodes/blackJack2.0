c9a86485df9e08721c302fd396add22890ca8654
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-23 10:55:30 -0700

    fix python 2 brainfart
    and spell suits correctly

--- foo.py
+++ foo.py
@@ -1,4 +1,4 @@
-suites = ["Hearts", "Diamonds", "Clubs", "Spades"]
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 cardnames = [("Ace", 11),
 			("Two", 2),
 			("Three", 3),
@@ -16,6 +16,6 @@ cardnames = [("Ace", 11),
 
 
 with open("cards.txt","w") as f:
-	cards = "\n".join([f"The {card[0]} of {suite},{card[1]}" for suite in suites for card in cardnames])
+	cards = "\n".join([f"The {name} of {suit}, {name}, {value}" for suit in suits for name, value in cardnames])
 	
 	f.write(cards)
