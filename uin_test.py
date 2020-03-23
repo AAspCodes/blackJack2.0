6ef15f77e1715933a7806ea53efb08c46e1d777e
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-23 10:56:23 -0700

    expand useful feedback form get_int

---
+++ uin_test.py
@@ -0,0 +1,6 @@
+from user_input import get_int, get_name
+
+
+
+# print(get_int(10,100, "number between 10 and 100: "))
+print(get_name("enter a name:  "))
--- user_input.py
+++ user_input.py
@@ -1,10 +1,25 @@
 def get_int(min, max, message):
+	
+	class TooLowException(Exception):
+		pass
+	
+	class TooHighException(Exception):
+		pass
+
 	while True:
 		try:
 			num = int(input(message))
 			if min <= num <= max:
 				return num
-			else:
-				raise Exception
+			elif min > num:
+				raise TooLowException()
+			elif max < num:
+				raise TooHighException()
+
+		except TooLowException:
+			print("That value was too low!")
+		except TooHighException:
+			print("That value was too High!")
 		except Exception as e :
-			print("that was not a possible input.")
+			print("That was not a possible input. Try an Integer.")
+
